d2b84040a486bac17502c95e1ce4419a0fa93ad3
Al-E-00 <mazzellaalessio6@gmail.com>
2022-06-09 14:26:18 +0200

    added row -> col where insert the img

--- js/app.js
+++ js/app.js
@@ -12,7 +12,8 @@ const app = new Vue(
     data: {
         message: "Hello world!!!",
         img: "https://images.unsplash.com/photo-1564053489984-317bbd824340?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1196&q=80",
-    }
+    },
+    
 }
 
 );
