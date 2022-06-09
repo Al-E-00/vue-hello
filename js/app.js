b2d3456199fe191f6f6cd3bc717383e133401a2b
Al-E-00 <mazzellaalessio6@gmail.com>
2022-06-09 14:04:52 +0200

    Creation HTML base -> link VueJS -> print message with VueJS

---
+++ js/app.js
@@ -0,0 +1,21 @@
+/* 
+Print on screen a message inside an h1, using data.
+
+BONUS: 
+add to the page an image, take it from data.
+ */
+
+
+const app = new Vue(
+    {
+    el: "#root",
+    data: {
+        message: "Hello world!!!",
+    }
+}
+
+);
+
+
+
+
